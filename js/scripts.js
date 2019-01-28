6708c4c6d9e74a02e74c9ae6b654d9786e6c7263
Aaron Taylor and Yulia Shidlovskaya <aaron@>
2019-01-28 11:51:14 -0800

    to do list with remove function

--- js/scripts.js
+++ js/scripts.js
@@ -13,8 +13,8 @@ ToDoList.prototype.assignId = function() {
   return this.currentId;
 }
 
-AddressBook.prototype.addItem = function(item) {
-  contact.id = this.assignId();
+ToDoList.prototype.addItem = function(item) {
+  item.id = this.assignId();
   this.items.push(item);
 }
 
@@ -64,3 +64,38 @@ ToDoList.prototype.changeStatusItem = function(id) {
   };
   return false;
 }
+
+$(document).ready(function(){
+  var listToDo = new ToDoList;
+
+  var addListItem = function(list, item) {
+    $("#" + list).append("<input type='checkbox' name='" + list + "' value=" + item.id.toString() + "><span>" + item.description + "</span>");
+  };
+
+  var removeListItems = function(list) {
+    $("input:checkbox[name=" + list + "]:checked").each(function() {
+      var id = parseInt($(this).val());
+      listToDo.deleteItem(id);
+      console.log(listToDo);
+      alert(1);
+      $(this).next().remove();
+      $(this).remove();
+    });
+
+  };
+
+  $("#form").submit(function(event){
+    var inputItem = $("#item").val();
+    var oneItem = new Item(inputItem);
+
+    listToDo.addItem(oneItem);
+    addListItem("todo", oneItem);
+
+    event.preventDefault();
+  });
+
+  $("#delete").click(function(){
+    removeListItems("todo");
+    removeListItems("done");
+  });
+});
